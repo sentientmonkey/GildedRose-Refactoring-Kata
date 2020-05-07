47acc0bb70414a252fb30f148cf22278c870b78e
Scott Windsor <swindsor@gmail.com>
2020-05-07 15:55:08 -0700

    Move to iterator, extract methods

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -2,56 +2,79 @@ package com.gildedrose
 
 class GildedRose(var items: Array<Item>) {
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
+        items.forEach { item ->
+            if (!isAgedBrie(item) && !isBackstagePass(item)) {
+                if (aboveMinimumQuality(item)) {
+                    if (!isLegendary(item)) {
+                        decreaseQuality(item)
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+                if (aboveMaximumQuality(item)) {
+                    increaseQuality(item)
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                    if (isBackstagePass(item)) {
+                        if (expiresIn(item, 11)) {
+                            if (aboveMaximumQuality(item)) {
+                                increaseQuality(item)
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                        if (expiresIn(item, 6)) {
+                            if (aboveMaximumQuality(item)) {
+                                increaseQuality(item)
                             }
                         }
                     }
                 }
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
+            if (!isLegendary(item)) {
+                age(item)
             }
 
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
+            if (isExpired(item)) {
+                if (!isAgedBrie(item)) {
+                    if (!isBackstagePass(item)) {
+                        if (aboveMinimumQuality(item)) {
+                            if (!isLegendary(item)) {
+                                decreaseQuality(item)
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality
+                        halfQuality(item)
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                    if (aboveMaximumQuality(item)) {
+                        increaseQuality(item)
                     }
                 }
             }
         }
     }
 
+    private fun isBackstagePass(item: Item) = item.name == "Backstage passes to a TAFKAL80ETC concert"
+    private fun isAgedBrie(item: Item) = item.name == "Aged Brie"
+    private fun isLegendary(item: Item) = item.name == "Sulfuras, Hand of Ragnaros"
+    private fun increaseQuality(item: Item) {
+        item.quality = item.quality + 1
+    }
+
+    private fun decreaseQuality(item: Item) {
+        item.quality = item.quality - 1
+    }
+
+    private fun halfQuality(item: Item) {
+        item.quality = item.quality - item.quality
+    }
+
+    private fun age(item: Item) {
+        item.sellIn = item.sellIn - 1
+    }
+
+    private fun aboveMinimumQuality(item: Item) = item.quality > 0
+    private fun isExpired(item: Item) = item.sellIn < 0
+    private fun aboveMaximumQuality(item: Item) = item.quality < 50
+    private fun expiresIn(item: Item, days: Int) = item.sellIn < days
 }
 
